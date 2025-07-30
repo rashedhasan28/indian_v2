2db426acb5e1641d00fef39319b84951cf581a8b
rashedhasan28 <142791531+rashedhasan28@users.noreply.github.com>
2025-07-30 16:30:36 +0600

    Update settings.py

--- bot/settings.py
+++ bot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0fiujq9@%qqm^7os978x%8s07ah9-17drpqer859epx7vo0t-1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['indian-v2.onrender.com']
 
 
 # Application definition
